4a34ef298f6fc6e8bccf4dda18278f21cc987f7e
Nicholas Folse <nickfolse@gmail.com>
2016-06-04 20:33:45 -0600

    Initial commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,9 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+$(document).ready(function() {console.log("ready");} );
+
+$(document).click(function(loc) {
+    console.log(loc);
+});
+
